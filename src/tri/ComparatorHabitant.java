18ad00f5c59e75f4ef3496d48e757f08dbe5a2e5
anisskiller <anissaallou36@gmail.com>
2018-11-28 20:43:39 +0100

    Update ComparatorHabitant.java

--- src/tri/ComparatorHabitant.java
+++ src/tri/ComparatorHabitant.java
@@ -11,10 +11,10 @@ public class ComparatorHabitant implements Comparator<Pays>{
 	@Override
 	public int compare(Pays o1, Pays o2) {
 		// TODO Auto-generated method stub
-		Pays country1 = (Pays) o1;
-		Pays country2 = (Pays) o2;
+		// Pays country1 = (Pays) o1;
+		// Pays country2 = (Pays) o2;
 		
-		int nbHabitants = country1.getNbHabitants().compareTo(country2.getNbHabitants());
+		int nbHabitants = o1.getNbHabitants().compareTo(o2.getNbHabitants());
 		return nbHabitants;
 	}
 
